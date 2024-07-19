c69572f5f85ba98aae24f085a0bb1fc95ecde701
Adeniran Shaddai <shaddaidan@gmail.com>
2024-07-19 11:24:52 +0100

    got data

---
+++ ietf_scraper/ietf_scraper/spiders/ietf.py
@@ -0,0 +1,15 @@
+import scrapy
+
+
+class IetfSpider(scrapy.Spider):
+    name = "ietf"
+    allowed_domains = ["pythonscraping.com"]
+    start_urls = ["https://pythonscraping.com/linkedin/ietf.html"]
+
+    def parse(self, response):
+        # title = response.css("span.title::text").get()
+        
+        title = response.xpath('//span[@class="title"]/text()').get()
+        subheading = response.xpath('//span[@class="subheading"]/text()').get()
+        return {"title": title} , {"subheading" : subheading }
+    
